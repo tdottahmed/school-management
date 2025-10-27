893e53ada677250a5d603b266f4c86f18b911c3a
Tanbir <pondit.tanbir@gmail.com>
2025-10-27 13:42:05 +0600

    add: App installed variable condition

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -39,18 +39,20 @@ class AppServiceProvider extends ServiceProvider
 
 
         // Share view for Common Data
-        $user_languages = Language::where('status', '1')->get();
-        $setting = Setting::where('status', '1')->first();
-        $topbarSetting = TopbarSetting::where('status', '1')->first();
-        $socialSetting = SocialSetting::where('status', '1')->first();
-        $schedule_setting = ScheduleSetting::where('slug', 'fees-schedule')->first();
-        $footer_pages = Page::where('language_id', Language::version()->id)
-            ->where('status', '1')
-            ->orderBy('id', 'asc')
-            ->get();
+        if (env('APP_INSTALLED', false)) {
+            $user_languages = Language::where('status', '1')->get();
+            $setting = Setting::where('status', '1')->first();
+            $topbarSetting = TopbarSetting::where('status', '1')->first();
+            $socialSetting = SocialSetting::where('status', '1')->first();
+            $schedule_setting = ScheduleSetting::where('slug', 'fees-schedule')->first();
+            $footer_pages = Page::where('language_id', Language::version()->id)
+                ->where('status', '1')
+                ->orderBy('id', 'asc')
+                ->get();
 
-        Config::set('app.timezone', $setting->time_zone);
+            Config::set('app.timezone', $setting->time_zone);
 
-        View::share(['setting' => $setting, 'user_languages' => $user_languages, 'schedule_setting' => $schedule_setting, 'topbarSetting' => $topbarSetting, 'socialSetting' => $socialSetting, 'footer_pages' => $footer_pages]);
+            View::share(['setting' => $setting, 'user_languages' => $user_languages, 'schedule_setting' => $schedule_setting, 'topbarSetting' => $topbarSetting, 'socialSetting' => $socialSetting, 'footer_pages' => $footer_pages]);
+        }
     }
 }
